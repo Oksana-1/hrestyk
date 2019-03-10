192b24910f11cf90c5be9d0d2a2e91b2f9a7bfc8
oksana-1 <4438488@gmail.com>
2019-03-10 22:04:06 +0200

    About page mark-up and styles.

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,8 +1,60 @@
 <template>
     <div class="about-content">
-        
+        <div class="about-top-img-fix">
+            <div class="about-top-img" style="background-image: url('src/assets/images/about-img.jpg')"></div>
+            <div class="about-top-text">
+                <div class="about-top-text-inner">
+                    <h1 class="about-title">Магазин "Хрестик"</h1>
+                    <div class="editor-content">
+                        <p>Наша історія бере початок у 2015 році. Саме тоді було виготовлена перша пара вишитих сережок в етно-стилі.</p> 
+                        <p>З того часу ми відшили і відправили до майбутніх власниць сотні замовлень в різні країни світу. 
+                        Високу якість і ексклюзивний дизайн наших виробів оцінили люди в Україні, Німеччині, Канаді, Сполучених Штатах і навіть в Австралії.</p>
+                        <p>Ми дуже пишаємося такою географією, і будемо робити все можливе, щоб її розширити.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="about-section">
+            <div class="c-box-1100">
+                <div class="c-cont">
+                    <div class="col-50">
+                        <div class="traditional-img-fix">
+                            <img class="fits" src="src/assets/images/traditional.jpg" alt="Традиції і сучасність у вишивці">
+                        </div>
+                    </div>
+                     <div class="col-50 traditional-text">
+                         <h2 class="common-title">Традиції і сучасність</h2>
+                         <div class="editor-content">
+                            <p>Головне джерело натхнення для нас - це традиційна українська вишивка з ії неймовірним символізмом і гармонією кольорів.</p>
+                            <p>Ми намагаємося долучити багатовікові українські візерунки й орнаменти до сьогодення. Більшість мотивів в наших роботах подаровані матінкою-природою (це сонце, квіти, вода, земля).</p>
+                            <p>Як і прадавні майстирині ми створюємо речі тільки вручну і тільки із натуральних матеріалів.</p>
+                            <p>Магазин "Хрестик" ніколи не був і, схоже, не буде комерційним проектом. Для нас важлива перш за все ідея, якість і неповторність наших робіт.</p>
+                           
+                         </div>
+                     </div>
+                </div>
+            </div>
+        </div>
+        <div class="goods-section">
+             <div class="c-box-1100">
+                 <div class="c-cont">
+                     <div class="col-50 goods-text-col">
+                         <h2 class="common-title">Майстерність у деталях</h2>
+                         <div class="editor-content">
+                            <p>Ми приділяємо особливу увагу деталям.</p>
+                            <p>Так, наприклад, всі наші сережки мають спеціальні стопери, аби не губилися.<br>
+                                А ланцюжки -  зручні защіпки-ґудзики, що зберігають Ваш манікюр.
+                            </p>
+                             <p>Наша закоханість у свою роботу не знає меж. Сподіваємося передати Вам краплинку цієї любові.</p>
+                         </div>
+                     </div>
+                </div>
+            </div>
+            <div class="goods-img-fix">
+                <div class="goods-img" style="background-image: url('src/assets/images/product.jpg');"></div>
+            </div>
+        </div>
     </div>
-
 </template>
 <script>
 export default {
