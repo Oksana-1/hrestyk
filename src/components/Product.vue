6876341225dfa284fa12ccf6866a50b09ac22092
Oksana <4438488@gmail.com>
2019-03-17 16:11:39 +0200

    inner page markup

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,19 +1,33 @@
 <template>
     <div class="product-content">
-        <h1>{{productInfo.productName}}. {{productInfo.productId}}</h1>
-        <div class="product-card-wrap">
-            <div class="product-card-inner">
-                <div class="product-img-fix">
-                    <div class="product-img"  v-bind:style="{ 'backgroundImage': 'url(' + productInfo.images.img1 + ')' }"></div>
-                </div>
-                <div class="product-info product-info-test">
-                    <div class="buy-btn-row">
-                        <div class="product-price">{{ productInfo.productPrice }} грн</div>
-                        <a href="#" class="hrestyk-btn-dark">
-                            <span>Купити</span>
-                        </a>
+        <div class="c-box-1100">
+            <h1 class="common-title">{{productInfo.productName}}. {{productInfo.productId}}</h1>
+            <div class="c-cont">
+                <div class="col-50">
+                    <div class="product-inner-slider">
+                        <div class="product-img-fix">
+                            <div class="product-img"  v-bind:style="{ 'backgroundImage': 'url(' + productInfo.images.img1 + ')' }"></div>
+                        </div>
                     </div>
-                </div> 
+                </div>
+                 <div class="col-50">
+                     <div class="editor-container" v-html="productInfo.productDescr">
+                         {{productInfo.productDescr}}   
+                    </div>
+                    <div class="descr-note">
+                        <p>Ми робимо все можливе, щоб показати справжні кольори на своїх фотографіях. 
+                        Але вони сильно залежать від освітлення, і звичайно кольори можуть дещо відрізнятися на різних моніторах.</p>
+                        <p>Будь ласка, майте це на увазі!</p>
+                    </div>
+                     <div class="product-info product-info-test">
+                        <div class="buy-btn-row">
+                            <div class="product-price">{{ productInfo.productPrice }} грн</div>
+                            <a href="#" class="hrestyk-btn-dark">
+                                <span>Купити</span>
+                            </a>
+                        </div>
+                    </div> 
+                 </div>
             </div>
         </div>
     </div>
