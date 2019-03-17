9619f277b69228aab177cc1f07d4d86f57e3fbc9
Oksana <4438488@gmail.com>
2019-03-17 11:17:10 +0200

    product-inner

--- src/components/Product.vue
+++ src/components/Product.vue
@@ -1,15 +1,14 @@
 <template>
     <div class="product-content">
-        <h1>Inner page for product</h1>
+        <h1>{{productInfo.productName}}. {{productInfo.productId}}</h1>
         <div class="product-card-wrap">
             <div class="product-card-inner">
                 <div class="product-img-fix">
-                    <div class="product-img" style="background-image: url('../../src/assets/images/files/product1.jpg');"></div>
+                    <div class="product-img"  v-bind:style="{ 'backgroundImage': 'url(' + productInfo.images.img1 + ')' }"></div>
                 </div>
                 <div class="product-info product-info-test">
-                    <div class="product-name">{{productId}}</div>
                     <div class="buy-btn-row">
-                        <div class="product-price">150 грн</div>
+                        <div class="product-price">{{ productInfo.productPrice }} грн</div>
                         <a href="#" class="hrestyk-btn-dark">
                             <span>Купити</span>
                         </a>
@@ -23,7 +22,8 @@
 export default {
     data(){
         return{
-            productId: this.$route.params.id               
+            productId: this.$route.params.id,
+            productInfo: {}        
         }
     },
     watch: {
@@ -31,6 +31,27 @@ export default {
             this.productId = to.params.id; 
         }
 
+    },
+    methods: {
+        fetchData(){
+            this.$http.get('')
+            .then(response =>{
+                return response.json();
+            }, error =>{
+                console.log(error);
+            })
+            .then(data => {
+                for(let key in data.products){
+                    if(this.productId === data.products[key].productId){
+                        this.productInfo = data.products[key];
+                    }
+                }
+                
+            });
+        }
+    },
+    created(){
+        this.fetchData();
     }
 }
 </script>
