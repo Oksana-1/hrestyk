df99c8691e8e8ccfe6fa8b1c1b6cd81c2ec9bcfe
oksana-1 <4438488@gmail.com>
2019-04-25 12:33:27 +0300

    reorganize header and footer

---
+++ src/components/Cart.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="hr-cart-content">
+
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
---
+++ src/components/Header.vue
@@ -0,0 +1,47 @@
+<template>
+    <div class="pc-header">
+        <div class="c-box-1100 hr-header-wrap">
+            <div class="nav-cont">
+                <div @click="navigateToHome()" class="logo-fix">
+                    <img class="fits" src="../assets/images/logo.png" alt="Logo"/>
+                </div>
+                <nav class="pc-nav">
+                    <ul>
+                        <router-link to="/" tag="li" active-class="active" exact><a>Головна</a></router-link>
+                        <router-link to="/catalog" tag="li" active-class="active" exact><a>Каталог</a></router-link>
+                        <router-link to="/about" tag="li" active-class="active" exact><a>Про нас</a></router-link>
+                        <router-link to="/delivery" tag="li" active-class="active" exact><a>Оплата та доставка</a></router-link>
+                    </ul>
+                </nav>
+            </div>
+            <div class="hr-cart-wrap">
+                <icon-base icon-name="cart" view-box="0 0 459.529 459.529" width="25" height="21"><icon-cart /></icon-base>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+    import IconBase from '../components/IconBase.vue';
+	import IconFb from '../components/icons/IconFb.vue';
+	import IconIg from '../components/icons/IconIg.vue';
+	import IconTwitter from '../components/icons/IconTwitter.vue';
+	import IconPhone from '../components/icons/IconPhone.vue';
+	import IconEmail from '../components/icons/IconEmail.vue';
+	import IconCart from '../components/icons/IconCart.vue';
+export default {
+   components: {
+        IconBase,
+        IconFb,
+        IconIg,
+        IconTwitter,
+        IconPhone,
+        IconEmail,
+        IconCart
+   },
+   methods: {
+       navigateToHome(){
+            this.$router.push({path: '/'});
+        }
+   }
+}
+</script>
