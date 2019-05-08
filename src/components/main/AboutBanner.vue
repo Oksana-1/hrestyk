511cb9ff8c350d8d6e6fb5b7fa18083109a11d64
Oksana <4438488@gmail.com>
2019-05-08 10:22:23 +0300

    fix small bug with router

--- src/components/main/AboutBanner.vue
+++ src/components/main/AboutBanner.vue
@@ -4,7 +4,7 @@
             <div class="about-img" style="background-image: url('../../../src/assets/images/about-img.jpg');"></div>
             <div class="about-content-cont">
                 <div class="title-big">Наша історія</div>
-                <router-link :to="'/about/'" class="hrestyk-btn-light hrestyk-btn-200"><span>Докладніше</span></router-link>
+                <router-link :to="'/about'" class="hrestyk-btn-light hrestyk-btn-200"><span>Докладніше</span></router-link>
             </div>
         </div>
     </div>
