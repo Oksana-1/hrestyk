cce691c0e8a2fd8fa82e8d758667036c56096f4b
Oksana-1 <4438488@gmail.com>
2022-06-16 21:00:37 +0300

    Replace baseUrl variables into .env

--- src/api/publicApi.js
+++ src/api/publicApi.js
@@ -2,15 +2,19 @@ import axios from "axios";
 import Product from "@/entities/Product";
 import Order from "@/entities/Order";
 
-export const BASE_HOST = "http://95.179.185.226:8085/";
+export const BASE_URL_HOST = process.env.VUE_APP_BASE_URL_HOST;
+export const BASE_URL_PORT = process.env.VUE_APP_BASE_URL_PORT;
 const API_VERSION = "api/v1/";
+const BASE_URL = `${BASE_URL_HOST}${
+  BASE_URL_PORT ? ":" + BASE_URL_PORT : ""
+}/${API_VERSION}`;
 
 const PRODUCTS_URL = "product/all";
 const SINGLE_PRODUCT_URL = "product/single/";
 const ADD_TO_CART = "/cart/add-product";
 const GET_CART = "/cart/show";
 
-axios.defaults.baseURL = BASE_HOST + API_VERSION;
+axios.defaults.baseURL = BASE_URL;
 
 export default class PublicApi {
   async getProducts() {
