16be031f363c69a306fb5c43c4dcfba816ce2d8a
Oksana-1 <4438488@gmail.com>
2022-07-26 15:12:16 +0300

    Add local storage helpers

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -1,3 +1,12 @@
 export const cloneObj = (obj) => {
   return JSON.parse(JSON.stringify(obj));
 };
+export const getLocalStorageItem = (name) => {
+  return window.localStorage.getItem(name);
+};
+export const setLocalStorageItem = (name, value) => {
+  window.localStorage.setItem(name, value);
+};
+export const removeLocalStorageItem = (name) => {
+  window.localStorage.removeItem(name);
+};
